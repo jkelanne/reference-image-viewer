72b2a40dd8fcd9636cc07138536ef4eba670c2a6
Jukka Kelanne <jukka.kelanne@diter.com>
2022-07-15 14:09:38 +0300

    Some changes
    Added feature for loading an image from command line argument.
    Added a preliminary zoom to view size feature.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use eframe::egui;
 use egui_extras::RetainedImage;
 //use image;
@@ -5,6 +6,7 @@ use egui_extras::RetainedImage;
 struct RefImageView {
     image: RetainedImage,
     image_scale: f32,
+    auto_resize: bool,
 }
 
 impl Default for RefImageView {
@@ -15,14 +17,39 @@ impl Default for RefImageView {
                 include_bytes!("../resources/FXm37GQaMAAAYs7.png"),
             ).unwrap(),
             image_scale: 0.25,
+            auto_resize: false, 
         }
     }   
+}
+
+fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, image::ImageError> {
+    let image = image::io::Reader::open(path)?.decode()?;
+    let size = [image.width() as _, image.height() as _];
+    let image_buffer = image.to_rgba8();
+    let pixels = image_buffer.as_flat_samples();
+
+    Ok(egui::ColorImage::from_rgba_unmultiplied(
+        size,
+        pixels.as_slice(),
+    ))
 }
 
 impl RefImageView {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         cc.egui_ctx.set_visuals(egui::Visuals::dark());
         Self::default()
+    }
+
+    fn new_with_colorimage(cc: &eframe::CreationContext<'_>, ci: egui::ColorImage, scale: f32) -> Self {
+        cc.egui_ctx.set_visuals(egui::Visuals::dark());
+        Self {
+            image: RetainedImage::from_color_image(
+                "filename",
+                ci,
+            ),
+            image_scale: scale,
+            auto_resize: false,
+        }    
     }
 }
 
@@ -37,57 +64,56 @@ impl eframe::App for RefImageView {
                 if ui.button("-").clicked() {
                     self.image_scale -= 0.01;
                 }
+
+                if ui.button("*").clicked() {
+                    self.auto_resize = true;
+                }
                 egui::widgets::global_dark_light_mode_switch(ui);
-/*                if ui.button("Dark").clicked() {
-                    ctx.set_visuals(egui::Visuals::dark());
-                }*/
             });
         });
 
-/*        egui::SidePanel::left("my_left_panel").show(ctx, |ui| {
-            ui.label("Hello World!");
-        });*/
-
         egui::CentralPanel::default().show(ctx, |ui| {
-            // self.image.show(ui);
-/*            ui.horizontal(|ui| {
-                if ui.button("+").clicked() {
-                    self.image_scale += 0.01;
+            if self.auto_resize {
+                let panel_ratio = ui.available_width() / ui.available_height();
+                //println!("ui width: {}; height: {}", ui.available_width(), ui.available_height());
+                let i_size = self.image.size();
+                let image_ratio = i_size[0] as f32 / i_size[1] as f32;
+
+                //println!("panel_ratio: {}; image_ratio: {}", panel_ratio, image_ratio);
+
+                if panel_ratio > image_ratio {
+                    self.image_scale = ui.available_height() / i_size[1] as f32;
+                } else {
+                    self.image_scale = ui.available_width() / i_size[0] as f32;
                 }
-
-                if ui.button("-").clicked() {
-                    self.image_scale -= 0.01;
-                }                
-            });*/
-
-
-
-            self.image.show_scaled(ui, self.image_scale);
-            // ui.heading("RefImageView");
-
-/*            ui.add(
-                egui::Image::new(self.image.texture_id(ctx), self.image.size_vec2())
-                    .rotate(45.0_f32.to_radians(), egui::Vec2::splat(0.5)),
-            );*/
-
-            /*ui.horizontal(|ui| {
-                ui.label("Hello World: ");
-                ui.text_edit_singleline(&mut self.name);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click here").clicked() {
-                self.age += 1;
+                //println!("self.image_scale: {}", self.image_scale);
             }
-            ui.label(format!("Hello {}", "world"));*/
+            self.image.show_scaled(ui, self.image_scale);
         });
     }
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
     let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Reference Image Viewer",
-        options,
-        Box::new(|cc| Box::new(RefImageView::new(cc))),
-    );
+
+    if args.len() > 1 {
+        if std::path::Path::new(&args[1]).exists() {
+            // Bake error handling into load_image_from_path()
+            let ci = load_image_from_path(std::path::Path::new(&args[1])).unwrap();
+
+            eframe::run_native(
+                "Reference Image Viewer",
+                options,
+                Box::new(|cc| Box::new(RefImageView::new_with_colorimage(cc, ci, 0.5))),
+            );
+        }    
+    } else {
+        eframe::run_native(
+            "Reference Image Viewer",
+            options,
+            Box::new(|cc| Box::new(RefImageView::new(cc))),
+        );
+    }
 }
