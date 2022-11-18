c257beeca9f58e2b6901ee63d29507f4a99c4a56
Jukka Kelanne <jukka.kelanne@diter.com>
2022-11-18 12:29:19 +0200

    Added get_conf_filename() to utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -9,6 +9,15 @@ pub fn get_tags_filename() -> String {
     tags_file
 }
 
+pub fn get_conf_filename() -> String {
+    let mut conf_file: String = String::new();
+    if let Some(proj_dirs) = ProjectDirs::from("com", "null ptr", "refiv") {
+        conf_file = proj_dirs.config_dir().to_owned().into_os_string().into_string().unwrap();
+        conf_file.push_str("\\conf.json");
+    }
+    conf_file
+}
+
 pub fn get_extension_from_filename(filename: &str) -> Option<&str> {
     std::path::Path::new(filename)
         .extension()
